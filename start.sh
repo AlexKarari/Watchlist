0821506cfe82b499a41d42150677472274f9b901
alexkarari <k.alexander@gmail.com>
2018-04-13 15:57:16 +0300

    Final push to heroku

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 export MOVIE_API_KEY=435074d7267ed8bc83984299b7d91b22
 export SECRET_KEY=secret
-
+export MAIL_USERNAME=lexkariuki92@gmail.com
+export MAIL_PASSWORD=15R@31@2014
 python3.6 manage.py server
